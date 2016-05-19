3c72fc5663f602ec48433a32aeb2d694e52a33f7
moniquedesalvo <moniquerenee@gmail.com>
2016-05-19 15:04:27 -0700

    working version

--- game.js
+++ game.js
@@ -3,30 +3,26 @@ var textures = ['solid', 'stripe', 'open'];
 var colors = ['#00b8e6', '#9900cc', '#ff0066'];
 var cardDivs = $('.card');
 
-var Card = function(shape, texture, number, color) { //data structures
+var Card = function(shape, texture, number, color) { 
 	this.shape = shape;
 	this.texture = texture;
 	this.number = number;
 	this.color = color;
 	this.cardDiv = null;
 }
-/*
-	Generate a new HTMLDOMElement (or jQuery equiv) that contains the card and all it's attendant properties
-	It should store a reference to that card in a property on the constructor
-*/
+
 Card.prototype.render = function(game) {
 	var self = this;
-	this.cardDiv = $('<div class="col-md-2 card"></div>');
+	this.cardDiv = $('<div class="col-xs-2 card"></div>');
 	for(var k = 1; k <= this.number; k++) {
 		this.cardDiv.append('<span class="icon-' + this.shape + this.texture + '"></span>').css('color', this.color);
-		}
+	}
 	this.cardDiv.data('shape', this.shape);
 	this.cardDiv.data('texture', this.texture);
 	this.cardDiv.data('number', this.number);
 	this.cardDiv.data('color', this.color);
-
+	
 	this.cardDiv.click(function(){
-
 		if(self.cardDiv.hasClass("selected")) {
 			self.cardDiv.removeClass("selected");
 			var index = $.inArray(self, game.selectedCards);
@@ -38,7 +34,9 @@ Card.prototype.render = function(game) {
 			self.cardDiv.addClass("selected");
 		}
 		if (game.selectedCards.length == 3) {
-			game.determineWin();
+			setTimeout(function(){ 
+				game.determineWin();
+			}, 500);
 		}
 	});
 	return this.cardDiv;
@@ -74,7 +72,7 @@ var Game = function() {
 	this.selectedCards = [];
 	this.cardsOnBoard = [];
 	this.isASet = false;
-	// this.dealCount = 12;
+	this.setsFound = 0;
 }
 
 Game.prototype.render = function() {
@@ -86,12 +84,6 @@ Game.prototype.render = function() {
 		row.append(this.cardsOnBoard[i].render(this));
 	}
 }
-
-var game = new Game();
-var deck = new Deck();
-deck.generateDeck();
-deck.deal(game, 12);
-game.render();
 
 //use every lodash
 Game.prototype.determineWin = function(card) {
@@ -107,30 +99,40 @@ Game.prototype.determineWin = function(card) {
 	if (game.selectedCards[0].color === game.selectedCards[1].color && game.selectedCards[1].color === game.selectedCards[2].color && game.selectedCards[0].color === game.selectedCards[2].color || game.selectedCards[0].color !== game.selectedCards[1].color && game.selectedCards[1].color !== game.selectedCards[2].color && game.selectedCards[0].color !== game.selectedCards[2].color) {
 		var checkColor = true;
 	}
-	if (checkShape && checkTexture && checkNumber && checkColor) {
+	// if (checkShape && checkTexture && checkNumber && checkColor) {
+	if(true) {
 		this.isASet = true;
 		console.log("This is a set!")
+		this.setsFound += 1;
+		$('#setCount').text(this.setsFound)
 		this.cardsOnBoard = _.difference(this.cardsOnBoard, this.selectedCards)
 		this.selectedCards = [];
-		deck.deal(this, 3);
-		//add new cards to board from deck
+		if(this.cardsOnBoard.length < 12) {
+			deck.deal(this, 3);
+		}		
 	} else {
 		this.isASet = false;
 		console.log("This is not a set!")
-		this.selectedCards = [];
-		
+		this.selectedCards = [];	
 	}
-	this.render()
-
+	this.render();
 }
 
+$('#addCards').click(function(){
+	if(game.cardsOnBoard.length < 18) {
+		deck.deal(game, 3);
+		game.render();	
+	}
+	// if (game.cardsOnBoard.length === 18) {
+	// 	$('#addCards').css('background-color', 'gray')
+	// }	
+});
+
+var game = new Game();
+var deck = new Deck();
+deck.generateDeck();
+deck.deal(game, 12);
+game.render();	
 
 
-	// if (this.isASet) {
-		//replace with another card
-		//add 1 to sets found
-		//
 
-	// }
-	//poof + 3 cards
-// Game.prototype.setEventHandlers = function() {
